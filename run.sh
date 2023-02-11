598a0d7478ad5602311dc88827d3c2647b429f55
Paul Clonts (Wilber) <pwc88430@uga.edu>
2023-02-10 23:14:51 -0500

    completed intersect method

---
+++ run.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+javac -d bin -cp bin src/cs2720/assignment1/ItemType.java
+javac -d bin -cp bin src/cs2720/assignment1/NodeType.java
+javac -d bin -cp bin src/cs2720/assignment1/SortedLinkedList.java
+javac -d bin -cp bin src/cs2720/assignment1/LinkedListDriver.java
+
+java -cp bin cs2720.assignment1.LinkedListDriver input.txt
